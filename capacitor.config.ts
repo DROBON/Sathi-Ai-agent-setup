69c17d78200010e2b9e1b5820088a56a8255e927
Abdur Rouf Rasel <84445833+DROBON@users.noreply.github.com>
2026-05-28 01:58:37 +0600

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,1 +1,12 @@
-jgxxhh
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sathi.ai.app',
+  appName: 'Sathi',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
